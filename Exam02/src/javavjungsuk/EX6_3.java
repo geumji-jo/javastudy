ba0584394f3f66ff3e1f2972ef3c2df7b733d4f3
tikitaca <sweet3761@naver.com>
2023-01-24 20:41:57 +0900

    2023-01-24

---
+++ Exam02/src/javavjungsuk/EX6_3.java
@@ -0,0 +1,21 @@
+package javavjungsuk;
+
+public class EX6_3 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+
+}
+class Card {
+	
+	int add(int a, int b) {
+		
+		int result = a + b;
+		return result;
+	}
+	
+
+	}
