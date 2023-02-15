088ff0896281932068cc62e6c61434531643e459
tikitaca <sweet3761@naver.com>
2023-02-15 17:23:19 +0900

    2023-02-15

--- Appendix03_Jdbc/src/ex01_connection/MainClass.java
+++ Appendix03_Jdbc/src/ex01_connection/MainClass.java
@@ -1,8 +1,12 @@
 package ex01_connection;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class MainClass {
 
@@ -56,9 +60,80 @@ public class MainClass {
 		
 	}
 	
+	public static void ex03() {
+		
+		BufferedReader reader = null;
+		Connection con = null;
+		
+		try {
+			
+			// 프로퍼티 파일을 읽는 문자 입력 스트림 생성하기
+			reader = new BufferedReader(new FileReader("db.properties"));
+			// 프로퍼티 파일을 읽어서 프로퍼티 객체 생성하기
+			Properties properties = new Properties();  // 우선 그냥 객체 생성해주기
+			properties.load(reader);  //load를 이용해서 reader를 불러오기 이 메소드는 try catch가 필요하다
+			
+			//프로퍼티 객체에 저장된 각 Property 읽기
+			String url = properties.getProperty("url");
+			String user = properties.getProperty("user");
+			String password = properties.getProperty("password");
+			
+			// DriverManaget로 부터 Connection 객체 얻기
+				con = DriverManager.getConnection(url, user, password);
+				System.out.println("DB에 접속되었습니다.");
+	
+			
+			
+}catch (IOException e) {
+			e.printStackTrace();
+}catch (SQLException e ) {
+			e.printStackTrace();
+	}finally {
+			
+			try {
+				
+			     if(con != null) {
+				    con.close();
+			     }
+			     
+		    }catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+	}
+	
+	public static Connection getConnection() {
+		
+		BufferedReader reader = null;
+		Connection con = null;
+		
+		try {
+			
+			
+			Class.forName("oracle.jdbc.OracleDriver");
+			
+			Properties properties = new Properties();
+					properties.load(new BufferedReader(new FileReader("db.properties")));
+							
+			con = DriverManager.getConnection(properties.getProperty("url"), properties.getProperty("user"), properties.getProperty("password"));
+
+		}catch(Exception e) {				// ClassNotFoundException, SQLException , ioException
+			e.printStackTrace();
+		}
+		
+		return con;
+		
+		
+		
+	}
+	
 	
 	public static void main(String[] args) {
-	ex02();
+	 Connection con = getConnection();
+	 System.out.println("DB에 접속되었습니다.");
+	 
 	
 
 	}
---
+++ Appendix03_Jdbc/src/ex01_connection/Sample.java
@@ -0,0 +1,42 @@
+package ex01_connection;
+
+public class Sample {
+
+	// 목록 조회
+	public void 목록조회하기() {
+		
+		// Connection 만들기
+		
+		// select
+		
+		// Connection 닫기
+		
+	}
+	
+	// 1개 조회
+	public void 한개조회하기() {
+		
+		// Connection 만들기
+		
+		// select
+		
+		// Connection 닫기
+		
+	}
+	
+	// 삽입
+	public void 삽입하기() {
+		
+	}
+	
+	// 수정
+	public void 수정하기() {
+		
+	}
+	
+	// 삭제
+	public void 삭제하기() {
+		
+	}
+	
+}
